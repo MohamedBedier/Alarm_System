3e17388759f8788415eaae78277d99021511c9d9
Abdullah AlsharQawy <abdullahalsharqwy@gmail.com>
2023-12-26 16:08:14 +0200

    Editing includes in SysTick_Prog.c file

--- Src/SysTick_Prog.c
+++ Src/SysTick_Prog.c
@@ -5,7 +5,7 @@
 ***********************************************************/
 
 #include <stdint.h>
-#include <Stm32F446xx.h>
+#include <Stm32F103C8T6.h>
 #include <ErrType.h>
 
 #include <SysTick_Interface.h>
