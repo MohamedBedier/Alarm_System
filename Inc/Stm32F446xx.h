5cdd942205e19f18c0e5f056e9badc4b29018cde
Abdullah AlsharQawy <abdullahalsharqwy@gmail.com>
2024-01-14 10:32:08 +0200

    edit Stm32F446xx.h

--- Inc/Stm32F446xx.h
+++ Inc/Stm32F446xx.h
@@ -7,6 +7,33 @@
 
 #define  SYSTICK_BASE_ADDRESS  0xE000E010U
 
+
+/************************		APB1 BASE ADRESSES		************************/
+
+#define     SPI2_BASE_ADDRESS        0x40003800U
+#define     SPI3_BASE_ADDRESS        0x40003C00U
+
+/************************		APB2 BASE ADRESSES		************************/
+
+#define     SPI1_BASE_ADDRESS        0x40013000U
+#define     SPI4_BASE_ADDRESS        0x40013400U
+
+
+/************************		SPI RESISTER STRUCTURE		************************/
+
+typedef struct
+{
+	volatile uint32_t SPI_CR1;
+	volatile uint32_t SPI_CR2;
+	volatile uint32_t SPI_SR;
+	volatile uint32_t SPI_DR;
+	volatile uint32_t SPI_CRCPR;
+	volatile uint32_t SPI_RXCRCR;
+	volatile uint32_t SPI_TXCRCR;
+	volatile uint32_t SPI_I2SCFGR;
+	volatile uint32_t SPI_I2SPR;
+}SPI_RegDef_t;
+
 /************************		SysTick RESISTER STRUCTURE		************************/
 
 typedef struct
@@ -18,13 +45,12 @@ typedef struct
 }SysTick_RegDef_t;
 
 
+
+
+
+
 /************************		SysTick Peripheral Definitions		************************/
 #define SYSTICK        ((SysTick_RegDef_t*)SYSTICK_BASE_ADDRESS)
-
-
-
-
-
 
 /************************		NVIC Peripheral Definitions		************************/
 #define NVIC_ISER      ((volatile uint32_t*)0xE000E100)
@@ -38,5 +64,10 @@ typedef struct
 /************************		SysCFG Peripheral Definitions		************************/
 #define SYSCFG_EXTICR      ((volatile uint32_t*)0x40013808)
 
+/************************		SPI Peripheral Definitions		************************/
+#define SPI1          ((SPI_RegDef_t*)SPI1_BASE_ADDRESS)
+#define SPI2          ((SPI_RegDef_t*)SPI2_BASE_ADDRESS)
+#define SPI3          ((SPI_RegDef_t*)SPI3_BASE_ADDRESS)
+#define SPI4          ((SPI_RegDef_t*)SPI4_BASE_ADDRESS)
 
 #endif
